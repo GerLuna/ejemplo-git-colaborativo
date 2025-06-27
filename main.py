bfc66e916b180649f8196acc23b9f6b9eeee97e2
GerLuna <gerardoluna522@gmail.com>
2025-06-26 23:59:48 -0600

    Agregar cambios main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from app.saludo import saludar
 
 if __name__ == "__main__":
-    print("Hola soy desconocido")
+    print("Hola desconocido")
     nombre = input("¿Cuál es tu nombre? ")
     saludar(nombre)

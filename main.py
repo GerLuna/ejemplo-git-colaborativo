7d3d03cff467ed046fbddd554621d2e1d7346e7a
Ulises Juarez <mmcjuarez@gmail.com>
2025-06-26 21:03:02 -0600

    Adicion de una funcion en la rama secundaria

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from app.saludo import saludar
 
+def segundoSaludo():
+    print("Hola soy el segundo saludo")
+
 if __name__ == "__main__":
     print("Hola soy desconocido")
     nombre = input("¿Cuál es tu nombre? ")
     saludar(nombre)
+    segundoSaludo()
